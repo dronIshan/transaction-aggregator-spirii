221dd8fb4d57677ceda7deb6c07617efd1908c21
dronIshan <56642293+dronIshan@users.noreply.github.com>
2025-07-01 17:23:49 +0300

    fix: removed extra shedule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { ScheduleModule } from '@nestjs/schedule';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ScheduleModule.forRoot(),
     TransactionsModule,
     AggregationModule,
   ],
